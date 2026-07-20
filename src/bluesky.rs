f65f6ca393acfe881a19c22a23ada8de0deaeb08
71104 <a71104@gmail.com>
2026-07-20 14:52:28 +0200

    Fix the BlueSky test vectors.

--- src/bluesky.rs
+++ src/bluesky.rs
@@ -110,9 +110,9 @@ mod tests {
                 from_const(2),
             ]),
             [
-                parse_scalar("0x6f30582cde48a25b26015b7f718ba2fb359e93029caf04d8d0b3e66b1d46b941"),
-                parse_scalar("0x5de8159372063ce76403529bb1a9725461b96467035d906400ff48d0937f9db6"),
-                parse_scalar("0x3c88b37dc6d14d08960b6fe58344e09194d11a930ce9f60cc90294683fac4b9f"),
+                parse_scalar("0x7b68dcd80fa751ee8f2d76043bfd92c685601c79189393fc76e03c5214eed32b"),
+                parse_scalar("0x0fbcb5720b463bf7e2ccabf373e77d2c10d27e6549f34cfa33eb2d06ea8b900a"),
+                parse_scalar("0x26e03abfcc62da0101516b07aede8bc676a10c47299a57bedc6d9fe80484f3da"),
             ]
         );
     }
@@ -127,10 +127,10 @@ mod tests {
                 from_const(3),
             ]),
             [
-                parse_scalar("0x775049834d9decb40ec5a109116a27527fa9105a3521cee8a42777788fda1501"),
-                parse_scalar("0x630ded08b39ceac4859c9ab6d14b548f48d01164ce1efada3a7a868f7d9248cb"),
-                parse_scalar("0x14b47f414dececb9936dcbb89e2fdd8511c44acb30439d1d23e48119b1c03b4f"),
-                parse_scalar("0x72de70292ce1ac7f30b859d04bbb6de5377288c1192a08863c34e11bc9269c4c"),
+                parse_scalar("0x12dde8a4c46760e349670d241e36ca7abacc991233039f8deaf6c58ce2230ef6"),
+                parse_scalar("0x61e95d9456e9223b4d7926dabae10009da2b6fb9134ade8405f6ef1424e66aa1"),
+                parse_scalar("0x2fcce25ab9efb3e26276f3b3aff1e02cdf82df48ce8d3eadbff900cfe015775b"),
+                parse_scalar("0x2580707d57a8c1c0cad368e8d5705ffd96f269d66e1cd6f1433f93a3c66d9bf8"),
             ]
         );
     }
@@ -140,14 +140,14 @@ mod tests {
         assert_eq!(
             hash_t3(&[from_const(42)]),
             [
-                parse_scalar("0x302e6d6d782c1367974698e051d9b55e18060b19393a4f0ac4b66f992bd5a5eb"),
-                parse_scalar("0x26f778c0f82ffe3d4409ebb9d7e4611556ca89c6a3e1a77cf8b80528eb344777"),
-                parse_scalar("0x1e161d647427e32a02a7d21edee0ad9de18d7070b0e01faf4dfdc127a737826a"),
+                parse_scalar("0x73952c443e4710be4a4c01e20046008b477f0d6fef5d87409cdebc4cdff3490c"),
+                parse_scalar("0x05bf595cdacac4f9eba8679b69dcde4eeeca6db242005bf6b923fde28ea88a46"),
+                parse_scalar("0x55d401b5483d5c70951f30fa5031b3898c43e831d4a413cb4094c8fbbe1aa9de"),
             ]
         );
         assert_eq!(
             hash_t3_0(&[from_const(42)]),
-            parse_scalar("0x302e6d6d782c1367974698e051d9b55e18060b19393a4f0ac4b66f992bd5a5eb")
+            parse_scalar("0x73952c443e4710be4a4c01e20046008b477f0d6fef5d87409cdebc4cdff3490c")
         );
     }
 
@@ -156,14 +156,14 @@ mod tests {
         assert_eq!(
             hash_t3(&[from_const(1), from_const(2)]),
             [
-                parse_scalar("0x2a24882111b586a835203bdeb7a97d8489e410eadf12a495624f49b729528873"),
-                parse_scalar("0x69233d2461effb6b25dbec14086d466f3bf668ef2a38759fa5cb433bedf25778"),
-                parse_scalar("0x53ee1c0f7bf24718f3686f0c3d4635e1628b992e9fefa9c092daa50c7db87fb8"),
+                parse_scalar("0x28935bd3eba75f7b2d4f62babbd4e907b1ffcc28f73d1cae33654441a8a84023"),
+                parse_scalar("0x339d0e485d8fdfb8c3391182d457fa3e73f043f566af1463ab05e57045122519"),
+                parse_scalar("0x748c7c2396801d8341dabf06412a27b394cf5214bdc6adb86423d156a8c9ba02"),
             ]
         );
         assert_eq!(
             hash_t3_0(&[from_const(1), from_const(2)]),
-            parse_scalar("0x2a24882111b586a835203bdeb7a97d8489e410eadf12a495624f49b729528873")
+            parse_scalar("0x28935bd3eba75f7b2d4f62babbd4e907b1ffcc28f73d1cae33654441a8a84023")
         );
     }
 
@@ -172,14 +172,14 @@ mod tests {
         assert_eq!(
             hash_t3(&[from_const(3), from_const(4), from_const(5)]),
             [
-                parse_scalar("0x160be03feff499f1256ce2404ff9ee026fc378b6a91d434746bab98aafaecb63"),
-                parse_scalar("0x14a259b91d964d8263af60fc1325c4874c68e8fd9caef509cc07622fc17718fe"),
-                parse_scalar("0x16f44c455c39bdabc7b77e2dbf089a0b0ac88a31b0ae9d39755f14b41d039e0c"),
+                parse_scalar("0x2bfc323795d99f44817eaa143a7db00103ff1eae1bd67ee3ab3f5a1006c7695d"),
+                parse_scalar("0x5e7468521c84b23259b813d193017a2b3c7813ce82e94ce4cc74a8c527db0923"),
+                parse_scalar("0x674a798a6315099b5b65e5eb5dae5dadd4bacb7a1e2bcee58fc9f8c888cc596a"),
             ]
         );
         assert_eq!(
             hash_t3_0(&[from_const(3), from_const(4), from_const(5)]),
-            parse_scalar("0x160be03feff499f1256ce2404ff9ee026fc378b6a91d434746bab98aafaecb63")
+            parse_scalar("0x2bfc323795d99f44817eaa143a7db00103ff1eae1bd67ee3ab3f5a1006c7695d")
         );
     }
 
@@ -188,14 +188,14 @@ mod tests {
         assert_eq!(
             hash_t3(&[from_const(6), from_const(7), from_const(8), from_const(9)]),
             [
-                parse_scalar("0x63d491b523ae737f62f117ef5affb8353996b67034ddaeb8586b574678ab440a"),
-                parse_scalar("0x531109ee099551ccea55a61f6f7cab781bf0d3d0d0c4ba032476b65d1ebb9867"),
-                parse_scalar("0x0061a9e7aa5a891cf8313d667039d29f7b0535866baf43fc0a87547bafcff275"),
+                parse_scalar("0x06ea9f66eddb8f036b0d6201dcf6a8c610b8aca9371e2bfc7fbd1deb1e5bb158"),
+                parse_scalar("0x0bc4c477fdeee23bf2f139b12c2ea927d145f298e6204255cbad8461af9150c6"),
+                parse_scalar("0x4cb7444cd642180aeaa40e7ad201d8d1f78d7ee1366400dd80373ebb0ae9a2d0"),
             ]
         );
         assert_eq!(
             hash_t3_0(&[from_const(6), from_const(7), from_const(8), from_const(9)]),
-            parse_scalar("0x63d491b523ae737f62f117ef5affb8353996b67034ddaeb8586b574678ab440a")
+            parse_scalar("0x06ea9f66eddb8f036b0d6201dcf6a8c610b8aca9371e2bfc7fbd1deb1e5bb158")
         );
     }
 
@@ -210,9 +210,9 @@ mod tests {
                 from_const(14),
             ]),
             [
-                parse_scalar("0x329255ad3db8a69a50a2a1f63fb4046d06d5bc6de30bf79bfe4138f4c93201df"),
-                parse_scalar("0x6968c301186d76def97ee0d7bcc1f426b34df8f2e04a3afdeaa1acd8f9070d76"),
-                parse_scalar("0x728f9ec6a9a91884c02c22075f6c62c92c9b7d0e350c51db30b0f34d435b8f82"),
+                parse_scalar("0x05ae2c9b2bdbb5a64d4e838bd96b0b4c2366fc6d3cee4309793e01dfd2a589d1"),
+                parse_scalar("0x67de663ef4d5db733c68cae13b6bb28aa97d0fc904dccdfa80f4c9fae36f51d0"),
+                parse_scalar("0x78638cb8d9a1d4d3baf3f9bfe7a6d6d696794a5fe64f08ae5389f55f4455cf63"),
             ]
         );
         assert_eq!(
@@ -223,7 +223,7 @@ mod tests {
                 from_const(13),
                 from_const(14),
             ]),
-            parse_scalar("0x329255ad3db8a69a50a2a1f63fb4046d06d5bc6de30bf79bfe4138f4c93201df")
+            parse_scalar("0x05ae2c9b2bdbb5a64d4e838bd96b0b4c2366fc6d3cee4309793e01dfd2a589d1")
         );
     }
 
@@ -232,15 +232,15 @@ mod tests {
         assert_eq!(
             hash_t4(&[from_const(42)]),
             [
-                parse_scalar("0x109a9fd885b0047b036489dad6d0ca97749f6a9b21d9fc2c1cb7d25952e453a0"),
-                parse_scalar("0x203e5346a31efe538f826a34e87c285ef6cfe0ce12a0316a25cbd4e2326abd29"),
-                parse_scalar("0x4c53083849aedf3e11959d1dad010d2f1d2951adfdcce95f6a480666e63c5834"),
-                parse_scalar("0x4ba8b81b7409920310da246ad211df71f0c92b86d885e2b2850906a1c9fd0731"),
+                parse_scalar("0x2fdb574b84cca8f2c657ea588d8812bafbba305b7a9933728753de0fcf104c40"),
+                parse_scalar("0x732f901b286e0f3575ab52e19494406c38f3db3e06169143f4c0369b3ba58ed9"),
+                parse_scalar("0x24c8327a61a3bd811e04b11107609bd91b8916ab5cf53fe927edaa27a9e8d5da"),
+                parse_scalar("0x646361f44a66f48386f5b514de940951363a3394f5fb95488b87d26051249bba"),
             ]
         );
         assert_eq!(
             hash_t4_0(&[from_const(42)]),
-            parse_scalar("0x109a9fd885b0047b036489dad6d0ca97749f6a9b21d9fc2c1cb7d25952e453a0")
+            parse_scalar("0x2fdb574b84cca8f2c657ea588d8812bafbba305b7a9933728753de0fcf104c40")
         );
     }
 
@@ -249,15 +249,15 @@ mod tests {
         assert_eq!(
             hash_t4(&[from_const(1), from_const(2)]),
             [
-                parse_scalar("0x7c4e380d8a3935c0e8073420573f5b6aaf9ed2c727afc4da64f12401ab355faf"),
-                parse_scalar("0x14b0dda71f3fb062cc99121629f080541891b8be0e65ba858906cf0b648042ac"),
-                parse_scalar("0x5218f71044490008f3713824dfa6be57a708ad295ca8df9fb4176340d61fb681"),
-                parse_scalar("0x2c37a603216ece26caf18c1cc9a0e909262f61f014075d5eb975ed853bcaed6b"),
+                parse_scalar("0x33eaaa53f69ea75566e04bcb9318f965d5e74b68663bb4a09adfeeae27c752f4"),
+                parse_scalar("0x292ad2994473be89dbfec5185888d85924bfa0f64b3be556609bbde3bad4360c"),
+                parse_scalar("0x3f972105e69fcceafe6ce580dab417c50a34316d2de43d73a79f861ef55ca87a"),
+                parse_scalar("0x3d73ecf48e39063538b626bccc64b91586dd420903a68f6d99cab91b2b37dfea"),
             ]
         );
         assert_eq!(
             hash_t4_0(&[from_const(1), from_const(2)]),
-            parse_scalar("0x7c4e380d8a3935c0e8073420573f5b6aaf9ed2c727afc4da64f12401ab355faf")
+            parse_scalar("0x33eaaa53f69ea75566e04bcb9318f965d5e74b68663bb4a09adfeeae27c752f4")
         );
     }
 
@@ -266,15 +266,15 @@ mod tests {
         assert_eq!(
             hash_t4(&[from_const(3), from_const(4), from_const(5)]),
             [
-                parse_scalar("0x2582eca7bed4bca9d4326a9e2ca601e0b3779582bb5173318a4e19ab005e7495"),
-                parse_scalar("0x0307db21c6063767f309fb09afcc4f250cbaed3f1d0870cd56d3276b18ced8d5"),
-                parse_scalar("0x3024913685a18187c7ae50f1dcabe3ea2acb407fc3abd5d107bd79f3dbd2e90c"),
-                parse_scalar("0x4f3563e16ae6ba5cdc036289e47628fb890cd8e3e3295ee8206a31b19742b0a8"),
+                parse_scalar("0x5220b264d93b85d22b4eb5a19c53ebfd08e1702e00dc76de14603165663006ea"),
+                parse_scalar("0x664ca128f4f6f225f282a671b522c267389f30f01d858757a1f029941510d8ec"),
+                parse_scalar("0x38ef442cd0ce47da5e7fdd912edfc2a95a36409b142fd0f94545267af135bcfa"),
+                parse_scalar("0x5e0348d56101a88221d50aaf5c15264f7fbd6667649c41ed2884ea6c0bb0a613"),
             ]
         );
         assert_eq!(
             hash_t4_0(&[from_const(3), from_const(4), from_const(5)]),
-            parse_scalar("0x2582eca7bed4bca9d4326a9e2ca601e0b3779582bb5173318a4e19ab005e7495")
+            parse_scalar("0x5220b264d93b85d22b4eb5a19c53ebfd08e1702e00dc76de14603165663006ea")
         );
     }
 
@@ -283,15 +283,15 @@ mod tests {
         assert_eq!(
             hash_t4(&[from_const(6), from_const(7), from_const(8), from_const(9)]),
             [
-                parse_scalar("0x6b13720a0ebd34f13327023c0232a3a3421f88d50b627bacfd114491ae48bfaa"),
-                parse_scalar("0x235d36a318fbc8175bce6613d8b8812a1d8ab17c56a70565f8eb1253a248f5d0"),
-                parse_scalar("0x379747ade21215413ccf1e1d91c7f367e1d5d8cd3e52b24da10e080dc8b25c43"),
-                parse_scalar("0x0aaadc8d120b5ba7e6b10a0d0055eb8cce41ef0134a0c4a6d057bceec8433fae"),
+                parse_scalar("0x6cef40d837aeb6183356cf40d9818bc0ee109c557b17bffd80ab2905e4e2292f"),
+                parse_scalar("0x688cf6e7f2aba6c399bc3253ce3827f7a003f8170fe679cbcc2b37e9ba65211e"),
+                parse_scalar("0x1d14218c5f5ae32b4fc20b250b52ad8ec96a77627a6c103c8ecf3919290d6239"),
+                parse_scalar("0x1c73c75b6f00cabb2ba253363ead5515a9ffb8a6b9296cc6bb1faecc13e4e322"),
             ]
         );
         assert_eq!(
             hash_t4_0(&[from_const(6), from_const(7), from_const(8), from_const(9)]),
-            parse_scalar("0x6b13720a0ebd34f13327023c0232a3a3421f88d50b627bacfd114491ae48bfaa")
+            parse_scalar("0x6cef40d837aeb6183356cf40d9818bc0ee109c557b17bffd80ab2905e4e2292f")
         );
     }
 
@@ -306,10 +306,10 @@ mod tests {
                 from_const(14),
             ]),
             [
-                parse_scalar("0x4f07a42cf3cd73f35eeb9b42bff06b11e1c7ebe0fd8f65b7fab0dd5d551f1c6c"),
-                parse_scalar("0x2507a1f641f6bab3bb2cc40cb6d14df149aeede849a53a4590d73b0d29af2d71"),
-                parse_scalar("0x79f1da2d204aa97c4256d321055eac279959efeff119a32e106220ef69699d4f"),
-                parse_scalar("0x3ba12f3c9a2916b9bd891512147dad141d049875c7d1d5119edf815eef7dc05d"),
+                parse_scalar("0x22c96d13097aa3b4782f9d2580dc2295378f87c85aaed5f47ee3f8b036faf8ee"),
+                parse_scalar("0x0bb3130cba6d1aa9cd4ac577dd503905305ce7ccc08d04ec15d9a9700eb747a1"),
+                parse_scalar("0x1cc1f59d0c8b31f60c5b10478b28db466bdcdefda0e8da296d96d5529177d621"),
+                parse_scalar("0x11986cd24911e4e8529f30918a734c160ef1f408854346d1e9472ab352668753"),
             ]
         );
         assert_eq!(
@@ -320,7 +320,7 @@ mod tests {
                 from_const(13),
                 from_const(14),
             ]),
-            parse_scalar("0x4f07a42cf3cd73f35eeb9b42bff06b11e1c7ebe0fd8f65b7fab0dd5d551f1c6c")
+            parse_scalar("0x22c96d13097aa3b4782f9d2580dc2295378f87c85aaed5f47ee3f8b036faf8ee")
         );
     }
 }
